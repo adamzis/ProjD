ea859b916ca75c777a7cb2e0374b4386f48c2f5c
Adamzis <adam.j.a@hotmail.com>
2018-10-29 11:45:41 -0400

    V0.2 finished GPS
    -implement doGPS() method in engine. Calculates distance between two
    points on earth surface and returns whole number of it. (model)
    -Create jspx page for gps that takes 4 numbers (view)
    -Create Gps.java (controller)

--- ProjD/src/ctrl/Gps.java
+++ ProjD/src/ctrl/Gps.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Engine;
+
 /**
  * Servlet implementation class Gps
  */
@@ -16,7 +18,27 @@ public class Gps extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		if (request.getParameter("getGps") != null) {
+			Engine model = Engine.getEngine();
 
+			String fromLat = request.getParameter("fromLat");
+			String fromLong = request.getParameter("fromLong");
+			String toLat = request.getParameter("toLat");
+			String toLong = request.getParameter("toLong");
+
+			request.setAttribute("fromLat", fromLat);
+			request.setAttribute("fromLong", fromLong);
+			request.setAttribute("toLat", toLat);
+			request.setAttribute("toLong", toLong);
+
+			try {
+				String gps = model.doGps(fromLat, fromLong, toLat, toLong);
+				request.setAttribute("result", gps);
+			} catch (NumberFormatException e) {
+				request.setAttribute("error", e.getMessage());
+			}
+		}
+		this.getServletContext().getRequestDispatcher("/Gps.jspx").forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
--- ProjD/src/model/Engine.java
+++ ProjD/src/model/Engine.java
@@ -5,6 +5,8 @@ import java.math.BigInteger;
 public class Engine {
 
 	private static Engine singleEngine = null;
+	public static final int EARTH_DIAMETER = 12742;
+	public static final double CONVERT_TO_RADIANS = Math.PI / 180.0;
 
 	private Engine() {
 
@@ -21,17 +23,33 @@ public class Engine {
 
 		if (Long.parseLong(lowerBound) > Long.parseLong(upperBound))
 			throw new Exception();
-		
+
 		BigInteger primeInt;
 
 		primeInt = new BigInteger(lowerBound);
 		primeInt = primeInt.nextProbablePrime();
-		
+
 		if (primeInt.longValue() > Long.parseLong(upperBound))
 			throw new Exception();
 
 		return primeInt.toString();
 
+	}
+
+	public String doGps(String fromLat, String fromLong, String toLat, String toLong) throws NumberFormatException {
+
+		double lat1 = Double.parseDouble(fromLat) * CONVERT_TO_RADIANS;
+		double long1 = Double.parseDouble(fromLong) * CONVERT_TO_RADIANS;
+		double lat2 = Double.parseDouble(toLat) * CONVERT_TO_RADIANS;
+		double long2 = Double.parseDouble(toLong) * CONVERT_TO_RADIANS;
+
+		double Y = Math.cos(lat1) * Math.cos(lat2);
+		double X = Math.pow(Math.sin((lat2 - lat1) / 2), 2.0) + Y * Math.pow(Math.sin((long2 - long1) / 2), 2.0);
+
+		double distance = EARTH_DIAMETER * Math.atan2(Math.sqrt(X), Math.sqrt(1 - X));
+
+		long longDistance = Math.round(distance);
+		return Long.toString(longDistance);
 	}
 
 }
